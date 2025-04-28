c5403182d0ea5934b4ab1bbb70177cd5b9ed1fc9
Eugene Romanov <72860505+EugeneRomanov@users.noreply.github.com>
2025-04-28 18:38:06 +0300

    add project

---
+++ src/forecasters/__init__.py
@@ -0,0 +1,5 @@
+from .best_forecaster import BestForecaster
+from .classical_forecaster import ClassicalForecaster
+from .modern_forecaster import CatBoostForecaster 
+
+__all__ = ['BestForecaster', 'ClassicalForecaster', 'CatBoostForecaster']
